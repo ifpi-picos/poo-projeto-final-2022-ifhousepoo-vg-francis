4d87d9558379cd7c946a24bd56a41881806cc85d
Victor Gabriel <capic.2022118TADS0021@aluno.ifpi.edu.br>
2022-11-19 11:00:01 -0300

    initial commit

---
+++ src/main/java/com/example/UsuarioProprietario.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.sql.Date;
+
+public class UsuarioProprietario extends Usuario {
+
+    public UsuarioProprietario(String nome, String email, Endereco endereco, Date dataNasciemento) {
+        super(nome, email, endereco, dataNasciemento);
+    }
+    
+    
+}
