6ce4f645f6f74ddf3cf0c65ce3ece5bbcb099001
Victor Gabriel <capic.2022118tads0021@aluno.ifpi.edu.br>
2023-01-17 15:40:43 -0300

    teste

--- database/poodatabase.sql
+++ database/poodatabase.sql
@@ -1,5 +1,5 @@
 CREATE TABLE usuario(
- id_usuario SERIAL INT PRIMARY KEY,
+ id_usuario SERIAL PRIMARY KEY,
  nome VARCHAR(40) NOT NULL,
  email VARCHAR(60) NOT NULL,
  data_nascimento DATE NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE Imovel (
  descricao VARCHAR(100) NOT NULL,
  dataCadastro date not null,
  endereco VARCHAR(100) NOT NULL,
- id_imovel INT PRIMARY KEY NOT NULL
+ id_imovel SERIAL PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE Endereco (
@@ -19,5 +19,5 @@ CREATE TABLE Endereco (
  bairro VARCHAR(100) NOT NULL,
  numero VARCHAR(100) NOT NULL,
  uf VARCHAR(100) NOT NULL,
- id_endereco INT PRIMARY KEY
+ id_endereco SERIAL PRIMARY KEY
 );
