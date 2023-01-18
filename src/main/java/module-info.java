b8af5cacb8a5ce18bb4a77dc37ef244665bca768
Francisco <francisco.carvalho.q@gmail.com>
2023-01-18 03:17:37 -0300

    remover dotenv

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module IFHouse {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.instrument;
-    requires dotenv.java;
     exports com.example to javafx.graphics, javafx.fxml;
     opens com.example to javafx.fxml;
     exports com.example.Controllers to javafx.fxml, javafx.graphics;
